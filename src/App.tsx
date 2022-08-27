b1a84a6d9eb9c74fda1ae70a214e1b9e78d1c972
Damian Montero <photoca@hotmail.com>
2022-08-27 19:11:08 -0400

    read me

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Share from "./components/Share"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Share description={"this is a basic share page"}/>
       </header>
     </div>
   );
